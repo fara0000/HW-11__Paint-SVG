6adf8d4297f5f4940db4f26b08c6fa8cc4b05e10
rauf-babayev <rauf.rb.business@gmail.com>
2020-04-21 23:17:30 +0400

    done

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -2,33 +2,36 @@ function Controller(model, view) {
     this._model = model;
     this._view = view;
     this._isPressed = false;
-    this._color = '';
-    this._width = 0;
-    this.points = '';
+    this._color = 'black';
+    this._width = "50";
+    this.point = '';
+    
 }
 
 Controller.prototype.init = function() {
     this._view.init();
-    this._view.onMouseDown(this.startDraw.bind(this));
     this._view.onMouseUp(this.stopDraw.bind(this));
     this._view.onMouseMove(this.isMouseDraw.bind(this));
     this._view.changeColoring(this.changeColor.bind(this));
     this._view.changeSize(this.changeWidth.bind(this));
+    this._view.onMouseDown(this.startDraw.bind(this));
 }
 
 Controller.prototype.startDraw = function(x, y) {
     this._isPressed = true;
-    this._view.addPolyline(x, y);
+    this._view.addPolyline(x, y, this._color, this._width);
 }
 
 Controller.prototype.isMouseDraw = function(x, y) {
-    this._view.drawLine(x, y);
-    this.points(x, y);
+    if (this._isPressed) {
+        this._view.drawLine(x, y);
+        this.point;
+    }
 }
 
 Controller.prototype.points = function(x, y) {
     if (this._isPressed) {
-        this.points += ` ${x},${y}`;
+        this.point += ` ${x},${y}`;
     }
 }
 
